74c12a5e1a28516e973387101345b3e8b169bcb6
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-03-15 19:36:50 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,82 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu= $("#nav-menu"),
+    navToggle= $("#nav-toggle"),
+    navClose = $("#nav-close")
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle){
+    navToggle.click(function () {
+       navMenu.css("bottom",0);
+    });
+}
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose){
+    navClose.click(function () {
+        navMenu.css("bottom",-220);
+    });
+}
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = $(".nav__link");
+
+navLink.click(function () {
+    navMenu.css("bottom",-220);
+});
+
+/*==================== SKILLS FOLD ====================*/
+$(".skills__arrow").each(function () {
+
+})
+
+
+/*$(".skills__header").mouseenter(function(){
+    if ($(".skills__list").css("height",0)){
+
+    }$(".skills__list").css("height",80)
+});
+
+$(".skills__header").mouseleave(function(){
+    if ($(".skills__list").css("height",80)){
+
+    }$(".skills__list").css("height",0)
+});*/
+
+/*==================== ACCORDION SKILLS ====================*/
+
+
+/*==================== QUALIFICATION TABS ====================*/
+$("#btnEducation").click(function () {
+    if ($("#work").css("display","block")){
+        $("#education").css("display","block");
+        $("#work").css("display","none");
+    }
+});
+
+$("#btnWork").click(function () {
+    if ($("#education").css("display","block")){
+        $("#education").css("display","none");
+        $("#work").css("display","block");
+    }
+});
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
