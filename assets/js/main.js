ccb320587ef94284644e625bdd84dd0d8030ab31
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-03-16 12:18:57 +0530

    services final

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,6 +82,19 @@ $(".services__modal-close").click(function () {
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop:true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
 
 
 /*==================== TESTIMONIAL ====================*/
