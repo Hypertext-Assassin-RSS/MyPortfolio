a4f7dbc5f25010f39df132ea09f21854af61fd03
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-09-08 08:32:47 +0530

    change loader

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,11 +32,21 @@ if (navClose){
     });
 }
 /*==================== LOADER ====================*/
-$(window).on('load',function(){
+$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+$(window).on('load', function(){
+    setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+    $( "#loadingDiv" ).fadeOut(500, function() {
+        // fadeOut complete. Remove the loading div
+        $( "#loadingDiv" ).remove(); //makes page more lightweight
+    });
+}
+/*$(window).on('load',function(){
     $(".loader_container").fadeOut(1000);
     $(".content").fadeIn(1000);
 
-});
+});*/
 /*==================== REMOVE MENU MOBILE ====================*/
 /*const navLink = $(".nav__link");
 
